0d1e4ce557880faed6ba5ae61ee27195bbe71396
Zichun Koh <zichun@gmail.com>
2022-12-04 13:19:53 +0800

    2022 day 4

--- aoc-2022/src/main.rs
+++ aoc-2022/src/main.rs
@@ -6,11 +6,12 @@ use std::io::{self, Read};
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
 
-    println!("{}", day03::part1(&input));
-    println!("{}", day03::part2(&input));
+    println!("{}", day04::part1(&input));
+    println!("{}", day04::part2(&input));
 }
