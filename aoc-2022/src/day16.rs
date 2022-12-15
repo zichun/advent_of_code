c729d7a5e6c9fea3392e2a83f185b4c6fba20983
Zichun Koh <zichun@gmail.com>
2022-12-15 12:12:39 -0800

    day16 prelude

---
+++ aoc-2022/src/day16.rs
@@ -0,0 +1,14 @@
+pub fn part1(input: &str) -> u32 {
+    0
+}
+
+pub fn part2(input: &str) -> u32 {
+    0
+}
+
+#[test]
+fn test() {
+    let input = "";
+    assert_eq!(part1(input), 0);
+    assert_eq!(part2(input), 0);
+}
