e0827c8d24f314071cf768fa754fa3a741087ca1
Zichun Koh <zichun@gmail.com>
2023-12-23 01:01:32 -0800

    wip

--- aoc-2023/src/day23.rs
+++ aoc-2023/src/day23.rs
@@ -106,79 +106,51 @@ fn part1(grid: &Grid) -> usize {
 #[aoc(day23, part2)]
 fn part2(grid: &Grid) -> usize {
     let (mr, mc) = grid.dimensions();
-
-    let mut q = Vec::new();
-    let mut visited = vec![vec![0; mc]; mr];
-
     let sc = grid.row(0).enumerate().find(|(_, ch)| **ch == '.').unwrap().0;
-    q.push((0, sc));
 
-    fn flood(grid: &Grid, q: &mut Vec<(usize, usize)>, visited: &mut Vec<Vec<usize>>, r: usize, c: usize, steps: usize, mut found: bool) {
-        if visited[r][c] > 0 {
+    let mut q = VecDeque::new();
+    let mut visited = vec![vec![false; mc]; mr];
+    q.push_back((0, sc));
+
+    fn flood(grid: &Grid, visited: &mut Vec<Vec<bool>>, r: usize, c: usize, steps: usize, interesting: &mut Vec<(usize, usize, usize)>, first: bool) {
+        if visited[r][c] && !first {
             return;
         }
-        if found && *grid.get(r, c) == '.' {
-            println!("{} {} {}", r, c, steps);
-            q.push((r, c));
-            return;
-        }
+        visited[r][c] = true;
 
-        visited[r][c] = steps;
-        found = found || char_to_dir(*grid.get(r, c)).is_some();
-
-        for dir in Direction::iter() {
-            if let Some((nr, nc)) = grid.coord_with_dir(r, c, dir) {
-                if *grid.get(nr, nc) == '#' {
-                    continue;
-                }
-                flood(grid, q, visited, nr, nc, steps + 1, found);
+        let dirs = Direction::iter().filter(|dir| {
+            if let Some((nr, nc)) = grid.coord_with_dir(r, c, *dir) {
+                *grid.get(nr, nc) != '#' && !visited[nr][nc]
+            } else {
+                false
             }
+        }).collect::<Vec<_>>();
+
+        if first || dirs.len() == 1 {
+            dirs.into_iter().for_each(|dir| {
+                let (nr, nc) = grid.coord_with_dir(r, c, dir).unwrap();
+                flood(grid, visited, nr, nc, steps + 1, interesting, false);
+            });
+        } else if dirs.len() > 1 {
+            interesting.push((r, c, steps));
         }
     }
 
-    fn get_incomings(grid: &Grid, visited: &Vec<Vec<usize>>, r: usize, c: usize) -> Vec<Option<usize>> {
-        Direction::iter().map(|dir| {
-            match grid.coord_with_dir(r, c, dir) {
-                Some((nr, nc)) => {
-                    match char_to_dir(*grid.get(nr, nc)) {
-                        Some(_) =>
-                            if visited[nr][nc] > 0 {
-                                Some(visited[nr][nc])
-                            } else {
-                                None
-                            },
-                        None => Some(0),
-                    }
-                },
-                None => Some(0),
-            }
-        }).collect::<Vec<_>>()
+    let mut graph: HashMap<(usize, usize), Vec<(usize, usize, usize)>> = HashMap::new();
+
+    while let Some((r, c)) = q.pop_front() {
+        // inner flood
+        let mut interesting = Vec::new();
+        println!("flooding {} {}",r, c);
+        flood(&grid, &mut visited, r, c, 0, &mut interesting, true);
+        interesting.into_iter().for_each(|(nr, nc, steps)| {
+            graph.entry((r, c)).or_default().push((nr, nc, steps));
+            graph.entry((nr, nc)).or_default().push((r, c, steps));
+            q.push_back((nr, nc));
+        });
     }
-
-    while !q.is_empty() {
-        let found = q.iter().enumerate().find(|(ind, (r, c))| {
-            let interesting_incoming = get_incomings(&grid, &visited, *r, *c);
-
-            interesting_incoming.iter().find(|incoming| incoming.is_none()).is_none()
-        }).map(|(ind, (r, c))| (ind, *r, *c));
-
-        let (r, c) = match found {
-            Some((ind, r, c)) => {
-                q.remove(ind);
-                (r, c)
-            },
-            None => q.pop().unwrap()
-        };
-        let interesting_incoming = get_incomings(&grid, &visited, r, c);
-        let cur = match interesting_incoming.into_iter().filter_map(|inc| inc).max() {
-            Some(m) => m + 1,
-            None => 1,
-        };
-        println!("processing {} {}", r, c);
-        flood(&grid, &mut q, &mut visited, r, c, cur, false);
-    }
+    println!("{:?}", graph);
 
     let ec = grid.row(mr - 1).enumerate().find(|(c, ch)| **ch == '.').unwrap().0;
-    let tr = visited[mr - 1][ec] - 1;
-    tr
+    0
 }
