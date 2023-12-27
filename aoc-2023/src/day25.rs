888d31badf9bca80d79a18069e18ac1bb7fb3e2d
Zichun Koh <zichun@gmail.com>
2023-12-28 00:11:30 +0800

    day25 improve datastructure

--- aoc-2023/src/day25.rs
+++ aoc-2023/src/day25.rs
@@ -3,89 +3,102 @@ use rand::Rng;
 
 #[derive(Clone)]
 struct Graph {
-    vcnt: usize,
-    nodes: HashMap<usize, usize>,
-    edges: Vec<(usize, usize)>,
+    nodes: Vec<usize>,
+    adj: Vec<Vec<usize>>,
+    vertex_cnt: usize,
+    edge_cnt: usize,
+//    edges: Vec<(usize, usize)>,
 }
 
 impl Graph {
-    fn merge(&mut self, edge: usize) {
-        let node = self.vcnt;
-        self.vcnt += 1;
-        let (l, r) = self.edges[edge];
-        self.edges.remove(edge);
+    fn merge_edge(&mut self) {
+        let pick = rand::thread_rng().gen_range(0..self.edge_cnt);
 
-        let lcnt = self.nodes.remove(&l).unwrap();
-        let rcnt = self.nodes.remove(&r).unwrap();
-        self.nodes.insert(node, lcnt + rcnt);
+        let mut sofar = 0;
+        for i in 0..self.adj.len() {
+            if sofar + self.adj[i].len() > pick {
+                let rm = self.adj[i][pick - sofar];
+                self.nodes[rm] += self.nodes[i];
+                self.nodes[i] = 0;
 
-        let mut toadd = Vec::new();
-        let edges = self.edges.iter().filter(|(ef, et)| {
-            if *ef == l && *et == r {
-                false
-            } else if *ef == l || *ef == r || *et == l || *et == r {
-                toadd.push((*ef, *et));
-                false
-            } else {
-                true
+                for j in 0..self.adj[i].len() {
+                    let n = self.adj[i][j];
+                    if n != rm {
+                        self.adj[n].iter_mut().for_each(|nn| {
+                            if *nn == i {
+                                *nn = rm;
+                            }
+                        });
+                        self.adj[rm].push(n);
+                    }
+                }
+                self.adj[i].clear();
+
+                let br = self.adj[rm].len();
+                self.adj[rm].retain(|&x| x != i);
+                self.edge_cnt -= (br - self.adj[rm].len()) * 2;
+                self.vertex_cnt -= 1;
+
+                break;
+            } else if sofar > pick {
+                break;
             }
-        }).copied().collect::<Vec<_>>();
-
-        self.edges = edges;
-        toadd.into_iter().for_each(|(nl, nr)| {
-            if nl == l || nl == r {
-                self.edges.push((node.min(nr), node.max(nr)));
-            } else {
-                self.edges.push((node.min(nl), node.max(nl)));
-            }
-        });
-    }
-    fn pick_random(&self) -> usize {
-        rand::thread_rng().gen_range(0..self.edges.len())
+            sofar += self.adj[i].len();
+        }
     }
 }
 
 #[aoc_generator(day25)]
 fn parse(inp: &str) -> Graph {
     let mut nodes: HashMap<String, usize> = HashMap::new();
-    let mut vcnt = 0;
-    let mut edges = Vec::new();
+    let mut vertex_cnt = 0;
+    let mut edge_cnt = 0;
+    let mut adj = Vec::new();
 
     inp.lines().for_each(|l| {
         let mut l = l.split(": ");
         let from = l.next().unwrap();
         l.next().unwrap().extract_tokens::<String>()
             .for_each(|to| {
-                nodes.entry(from.to_owned()).or_insert_with(|| { vcnt += 1; vcnt - 1 });
-                nodes.entry(to.clone()).or_insert_with(|| { vcnt += 1; vcnt - 1 });
+                nodes.entry(from.to_owned()).or_insert_with(|| { vertex_cnt += 1; vertex_cnt - 1 });
+                nodes.entry(to.clone()).or_insert_with(|| { vertex_cnt += 1; vertex_cnt - 1 });
+
                 let (from, to) = (nodes[from], nodes[&to]);
-                edges.push((from.min(to), to.max(from)));
+                let (from, to) = (from.min(to), from.max(to));
+
+                while to >= adj.len() {
+                    adj.push(Vec::new());
+                }
+                adj[from].push(to);
+                adj[to].push(from);
+                edge_cnt += 2
             });
     });
     Graph {
-        vcnt,
-        nodes: (0..vcnt).map(|ind| (ind, 1)).collect(),
-        edges,
+        vertex_cnt,
+        nodes: vec![1; vertex_cnt],
+        adj,
+        edge_cnt,
     }
 }
 
 fn contract(graph: &mut Graph, t: usize)  {
-    while graph.nodes.len() > t {
-        graph.merge(graph.pick_random());
+    while graph.vertex_cnt > t {
+        graph.merge_edge();
     }
 }
 
 fn mincut(graph: &mut Graph) -> Option<usize> {
-    if graph.nodes.len() <= 6 {
+    if graph.vertex_cnt <= 6 {
         contract(graph, 2);
-        if graph.edges.len() == 3 {
-            let mut n = graph.nodes.iter();
-            Some(*n.next().unwrap().1 * *n.next().unwrap().1)
+        if graph.edge_cnt == 6 {
+            let mut n = graph.nodes.iter().filter(|x| **x != 0);
+            Some(*n.next().unwrap() * *n.next().unwrap())
         } else {
             None
         }
     } else {
-        let t = 1 + (graph.nodes.len() as f64 / (2.0_f64)).floor() as usize;
+        let t = 1 + (graph.vertex_cnt as f64 / (2.0_f64)).floor() as usize;
         let mut g2 = graph.clone();
         contract(graph, t);
         mincut(graph)
